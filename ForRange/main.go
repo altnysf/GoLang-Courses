5eee00ea8fdf3b609bde4d514a0fad1e3663037d
ysfaltn02 <yusuf@bereketaltun.com>
2022-08-23 01:27:54 +0300

    ForRange

--- ForRange/main.go
+++ ForRange/main.go
@@ -17,4 +17,19 @@ func main() {
 	Index is  3  and Product Name is  Keyboard
 	Index is  4  and Product Name is  Monitor
 	*/
+
+	numbers := [...]int{1, 2, 3, 4, 5}
+
+	for i := range numbers {
+		fmt.Println("Array Item ", i, " Value is ", numbers[i])
+	}
+
+	/*OUTPUTS
+	Array Item  0  Value is  1
+	Array Item  1  Value is  2
+	Array Item  2  Value is  3
+	Array Item  3  Value is  4
+	Array Item  4  Value is  5
+	*/
+
 }

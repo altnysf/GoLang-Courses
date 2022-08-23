a921ab39e96cfb4bff5f06af4f16ad7af52d996c
ysfaltn02 <yusuf@bereketaltun.com>
2022-08-23 12:35:04 +0300

    Arrays

---
+++ Arrays/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Basic Array Example
+
+	myArray := [3]int{}
+	myArray[0] = 32
+	myArray[1] = 64
+	myArray[2] = 52
+
+	fmt.Println(myArray)
+
+	fmt.Println(" -------------------------- ")
+
+	secondArray := [...]string{"red", "green", "blue", "brown"}
+	// [...] -> used for automatic sizing.
+	fmt.Println(secondArray)
+	fmt.Println("Current Value : ", secondArray[2]) // blue
+	secondArray[2] = "black"                        // SET
+	fmt.Println("New Value : ", secondArray[2])     //black
+}

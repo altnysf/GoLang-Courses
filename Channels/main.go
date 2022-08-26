6f872a4670efb2eea55f6164ecbd69d58141927e
Yusuf ALTUN <yusuf@bereketaltun.com>
2022-08-26 17:59:31 +0300

    Channels

---
+++ Channels/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{1, 2, 3, 4, 5, 6}
+	c := make(chan int)
+	go sum(s[:len(s)], c)
+
+	x := <-c
+
+	fmt.Println(x)
+}
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
